c701d02388e5cd36d6286cf61d9a018134da0bb5
Alabandit-AB <alastair@afrikaburn.com>
2016-09-08 09:03:06 +0000

    General update

--- sites/all/themes/afrikaburn/templates/html.tpl.php
+++ sites/all/themes/afrikaburn/templates/html.tpl.php
@@ -55,6 +55,14 @@
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7&appId=218398015032600";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
     <div id="wrapper">
         <?php print $page_top; ?>
         <?php print $page; ?>
