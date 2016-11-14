23708fec40e95044a4059350344eacfd0ed38037
Ubuntu <ubuntu@ip-172-31-26-221.us-west-2.compute.internal>
2016-11-14 13:48:38 +0000

    General update

---
+++ sites/all/modules/export_doc/templates/print_node.tpl.php
@@ -0,0 +1,7 @@
+<?php
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix">
+  <h1 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <div class="content clearfix">
+    <?php print $content; ?>
+  </div>
+</div> <!-- /.node -->
