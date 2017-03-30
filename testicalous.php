47790099b0c005bbc4102bfe9934a5b4fa913343
Ubuntu <ubuntu@ip-172-31-26-1.us-west-2.compute.internal>
2017-03-30 15:39:24 +0000

    General update

---
+++ testicalous.php
@@ -0,0 +1,19 @@
+<?php
+$headers = array(
+    'Content-Type' => 'application/json',
+    'usertoken' => '9KySOwx7LTxATa4ODyTeYnJ57kDBVErT',
+);
+$user_pass='';
+$data = "";
+
+$options = array(
+  'method' => 'GET',
+  'data' => $data,
+  'timeout' => 45,
+  'headers' => $headers,
+);
+
+$result = drupal_http_request('https://api.quicket.co.za/api/events/26935', $options);
+drupal_set_message('Result:<br><pre>'. print_r($result->data, true) .'</pre>'); 
+
+?>
