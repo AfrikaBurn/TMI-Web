7e2908606d2ee1a965795a7e6ebbb63f2f499d63
Scheepers <scheepers.debruin@gmail.com>
2020-01-28 15:58:20 +0200

    Collapsibleness

--- web/themes/custom/burnt/js/burnt.js
+++ web/themes/custom/burnt/js/burnt.js
@@ -9,6 +9,30 @@
 
   Drupal.behaviors.burnt = {
     attach: function (context, settings) {
+
+      $(
+        function(){
+
+          var
+            $parents = $('.view-book-pages li').has('ul'),
+            $currentParents = $('.view-book-pages .current').parents('li')
+
+          $parents
+            .addClass('collapsible-book-page')
+            .not($currentParents)
+            .addClass('collapsed-book-page')
+
+          $('.collapsible-book-page > .views-field .field-content').click(
+            function(){
+              $(this)
+                .parents('.collapsible-book-page')
+                .first()
+                .toggleClass('collapsed-book-page')
+            }
+          )
+
+        }
+      )
     }
   }
 
